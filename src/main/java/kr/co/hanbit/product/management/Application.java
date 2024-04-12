fb044cd270c15c8d92993d4570add81e13aa8669
hongkikii <hyangmeeh@icloud.com>
2024-04-12 23:45:15 +0900

    chore: mysql 연결 설정

--- src/main/java/kr/co/hanbit/product/management/Application.java
+++ src/main/java/kr/co/hanbit/product/management/Application.java
@@ -1,7 +1,10 @@
 package kr.co.hanbit.product.management;
 
+import java.sql.Connection;
+import javax.sql.DataSource;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,4 +25,11 @@ public class Application {
 		return modelMapper;
 	}
 
+	@Bean
+	public ApplicationRunner runner(DataSource dataSource) {
+		return args -> {
+			// 커넥션풀은 처음 사용되는 시점에 생성
+			Connection connection = dataSource.getConnection();
+		};
+	}
 }
